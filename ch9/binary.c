a490d2280e3fb8e0acd85b61a1aa0528f7feb800
Aaron Cohen <acohen@mountainpasstech.com>
2022-03-08 14:39:57 +0200

    add binary

--- ch9/binary.c
+++ ch9/binary.c
@@ -1,37 +1,25 @@
-// Write a program that reads input as a stream of characters until encoutering EOF. Have
-// it report the average number of letters per word. Don't count whitespace as being letters
-// in a word. Actually, punctuation shouldn't be counted either, but don't worry about that
-// now. (If you do want to worry about it, consider using the ispunct() function from the
-// ctype.h family.)
-
-// foo bar aaron letters/num_words 
 #include <stdio.h>
-#include <stdlib.h> 
-#include <ctype.h> 
 
+void to_binary(unsigned long n);
 
 int main(void) {
-	int ch;
-	int letters = 0;
-	int num_words = 0;
-	int average_letters_per_word = 0;
-	
-
-	while ((ch = getchar()) != EOF) {
-		if (ispunct(ch)) {
-			continue;
-		}
-		if (isspace(ch)) {
-			// new word.
-			num_words++;
-		} else {
-			// another character of the same word.
-			letters++;
-		}
+	unsigned long number;
+	printf("Enter an integer (q to quit):\n");
+	while (scanf("%lu", &number) == 1) {
+		printf("Binary:\n");
+		to_binary(number);
+		putchar('\n');
 	}
-	printf("Total words: %d\n", num_words);
-	printf("Total letters: %d\n", letters);
-	printf("Average letters per word: %d\n", letters/num_words);
-
+	
 	return 0;
+}
+
+void to_binary(unsigned long n) {
+	int r;
+	r = n %2;
+	if (n >= 2) {
+		to_binary(n/2);
+	}
+	putchar(r == 0 ? '0' : '1');
+	return;
 }
