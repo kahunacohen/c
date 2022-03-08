ea39d0a417b7853f1efcdddb7afa3fe3f72c8ac9
Aaron Cohen <acohen@mountainpasstech.com>
2022-03-08 15:09:05 +0200

    add parameter

--- ch9/binary.c
+++ ch9/binary.c
@@ -1,7 +1,7 @@
 /**
  * A recursive function to convert decimal to any number system, e.g. hex.
  * In this case a recursive function is useful because the first digit we find
- * is the last digit we want to print. E.g. to_binary(4): 100.
+ * is the last digit we want to print. E.g. to_base(4): 100.
  * 
  * 1. Remainder 4 % 2 == 0
  * 2. Recurse, passing 2
@@ -15,21 +15,21 @@
  */
 #include <stdio.h>
 
-void to_binary(unsigned long n);
+void to_base(unsigned long n, int base);
 
 int main(void) {
 	unsigned long number;
+	int base;
 	printf("Enter an integer (q to quit):\n");
-	while (scanf("%lu", &number) == 1) {
-		printf("Binary:\n");
-		to_binary(number);
+	while (scanf("%lu %d", &number, &base) == 2) {
+		to_base(number, base);
 		putchar('\n');
 	}
 	
 	return 0;
 }
 
-void to_binary(unsigned long n) {
+void to_base(unsigned long n, int base) {
 	int r;
 	// Determine if the number is odd or even.
 	// In binary even numbers will end in 0, odd inq
@@ -37,8 +37,8 @@ void to_binary(unsigned long n) {
 
 	// The base case is when n < 2 because, for example, if n
 	// is 1 or 0 there's nothing more to do.
-	if (n >= 2) {
-		to_binary(n/2);
+	if (n >= base) {
+		to_base(n/base, base);
 	}
 	putchar(r == 0 ? '0' : '1');
 	return;
