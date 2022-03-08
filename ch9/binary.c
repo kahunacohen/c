1c7cd06fb41c35313614578d813e5fb1dc0e52fa
Aaron Cohen <acohen@mountainpasstech.com>
2022-03-08 15:00:23 +0200

    add comments

--- ch9/binary.c
+++ ch9/binary.c
@@ -1,3 +1,18 @@
+/**
+ * A recursive function to convert decimal to any number system, e.g. hex.
+ * In this case a recursive function is useful because the first digit we find
+ * is the last digit we want to print. E.g. to_binary(4): 100.
+ * 
+ * 1. Remainder 4 % 2 == 0
+ * 2. Recurse, passing 2
+ * 3. 2 % 2 == 0
+ * 4. Recruse, passing 1
+ * 5. 1 is not greater than or equal to 2, so
+ * 6. 1 is odd so print 1
+ * 7. Unwrap stack frame:
+ * 8. print 0
+ * 9. print 0.
+ */
 #include <stdio.h>
 
 void to_binary(unsigned long n);
@@ -16,7 +31,12 @@ int main(void) {
 
 void to_binary(unsigned long n) {
 	int r;
+	// Determine if the number is odd or even.
+	// In binary even numbers will end in 0, odd inq
 	r = n %2;
+
+	// The base case is when n < 2 because, for example, if n
+	// is 1 or 0 there's nothing more to do.
 	if (n >= 2) {
 		to_binary(n/2);
 	}
